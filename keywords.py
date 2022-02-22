368bac93a14a825b6df166e03d2235f13e69054d
hemanth84-del <99811399+hemanth84-del@users.noreply.github.com>
2022-02-22 17:49:34 +0530

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,11 @@
+import keyword
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+# def = 'hello'
+
+print(keyword.iskeyword('aaz'))
+
+print(keyword.softkwlist)
+print(len(keyword.softkwlist))
+case = 10
+print(case)
