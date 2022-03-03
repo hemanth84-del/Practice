cac36cb4977745960a4bddbc7d1230a0708c91d8
hemanth84-del <99811399+hemanth84-del@users.noreply.github.com>
2022-03-03 18:01:16 +0530

    Add files via upload

---
+++ neon.py
@@ -0,0 +1,29 @@
+from math import Square as sqr
+print('this code is to check if a number is neon or not')
+def main():
+    num=int(input("Enter a number:"))
+    is_neon(num)
+
+def is_neon(num):
+    if num==neon(num):
+        print("It is a Neon Number.")
+    else:
+        print("It is not a Neon Number.")
+
+def neon(num):
+    res=0
+    count_digits=count_digits(num)
+    while num:
+        last_digit= num % 10
+        res += int(sqr(last_digit,count_digits))
+        num=remove_last_digit(num)
+    return res
+def count_digits(num):
+    count=0
+    while num:
+        count += 1
+        num=remove_last_digit(num)
+    return count
+def remove_last_digit(num):
+    return num//10
+main()
